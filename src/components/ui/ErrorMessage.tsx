13e492e657ae733cb77fb8334d69f861e80b05ad
vasily <vasilymishanin@gmail.com>
2023-10-25 10:30:54 +0600

    layout fix

--- src/components/ui/ErrorMessage.tsx
+++ src/components/ui/ErrorMessage.tsx
@@ -7,7 +7,7 @@ function ErrorMessage({ text, isHidden }: ErrorMessageProps) {
   return (
     <div
       className={`z-0 absolute bottom-36 w-[284px] h-[52px] text-accent_red font-bold flex items-center justify-center ${
-        isHidden ? 'opacity-0 -left-72' : ''
+        isHidden ? 'opacity-0 top-0' : ''
       }`}
     >
       <span>{text}</span>
