0e0a3d2d51272017ab459fe160dead4ca035ceef
vasily <vasilymishanin@gmail.com>
2023-10-26 16:11:59 +0600

    fix: netlify call node-fetch 1

--- netlify/functions/numVerifyProxy.ts
+++ netlify/functions/numVerifyProxy.ts
@@ -26,11 +26,18 @@ import { Config, Context } from '@netlify/functions';
 export default async (req: Request, context: Context) => {
   const { number, countryCode } = await req.json();
   const apiURL = Netlify.env.get('VITE_NUMVERIFY_URL');
+  if (!apiURL) {
+    throw new Error('No VITE_NUMVERIFY_URL in Evnironment variables');
+  }
+  const response = await fetch(apiURL);
+  const verificationResult = await response.json();
+
   const body = {
     message: 'Number Verification Request',
-    apiURL: apiURL,
-    number: number,
-    countryCode: countryCode,
+    apiURL,
+    number,
+    countryCode,
+    verificationResult,
   };
 
   return new Response(JSON.stringify(body));
