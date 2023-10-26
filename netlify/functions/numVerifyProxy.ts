2c3088b6d2012922c4a87bccd3c11d33ac0f2849
vasily <vasilymishanin@gmail.com>
2023-10-26 12:07:28 +0600

    fix: netlify functions internal error try fix

--- netlify/functions/numVerifyProxy.ts
+++ netlify/functions/numVerifyProxy.ts
@@ -26,8 +26,9 @@ import { Config, Context } from '@netlify/functions';
 export default async (req: Request, context: Context) => {
   const { number, countryCode } = context.params;
   const apiURL = Netlify.env.get('VITE_NUMVERIFY_URL');
+  const REQ = req.json();
   return new Response(
-    `You're searching for ${number} in ${countryCode} from ${apiURL}!`
+    `${REQ} ===> You're searching for ${number} in ${countryCode} from ${apiURL}!`
   );
 };
 
